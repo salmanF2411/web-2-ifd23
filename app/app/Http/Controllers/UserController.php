9a99c387e30cdae897a3c0a741dda82c82ca71ed
Finsa Nurpandi <finsaworks@gmail.com>
2026-05-06 12:12:10 +0700

    feat(softdelete dan mailable)

--- app/app/Http/Controllers/UserController.php
+++ app/app/Http/Controllers/UserController.php
@@ -8,13 +8,38 @@ use App\Models\Lecturer;
 use App\Models\Department;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\StoreLecturerRequest;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
 
 class UserController extends Controller
 {
     public function index()
     {
         $data['users'] = User::query()->with('department')->paginate(7);
-        
+        // $data['users'] = User::withTrashed()->with('department')->paginate(7);
+
+        // withTrashed() menyertakan data beserta baris yang telah dihapus
+        // onlytrashed() hanya menampilkan data yang telah dihapus
+        // restore() mengembalikan data (set deleted_at to null)
+        // forceDelete() delete dari basis data
+        // restoreAll() mengembalikan semua data
+        // forceDeleteAll() menghapus keseluruhan data dari basis data
+        $penerima = [
+            'mail1@mail.com',
+            'mail2@mail.com',
+            'mail3@mail.com',
+            'mail4@mail.com',
+            'mail5@mail.com',
+            'mail6@mail.com',
+            'mail7@mail.com',
+            'mail8@mail.com',
+            'mail9@mail.com',
+            'mail10@mail.com',
+        ];
+        // foreach($penerima as $pen) {
+        //     Mail::to($pen)->send(new TestMail());
+        // }
+
         return view('user', $data);
     }
 
--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -10,11 +10,12 @@ use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use App\Enums\DepartmentEnum;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
